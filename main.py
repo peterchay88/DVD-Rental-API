463b33fc88fdd66bd3e4cab9b852195b86483ae2
Peter Chay <peterchay88@gmail.com>
2026-08-04 17:01:53 -0600

    Updating main to use context manager for DB engine

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from db.session import engine
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
